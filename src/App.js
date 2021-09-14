7b73f0541f1783dbd8a3f1636d58798372921178
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-09-14 13:31:14 +1000

    change inital code from App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import SecondSection from "./screens/SecondSection";
+import TopSection from "./screens/TopSection";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="screen">
+        <Navbar />
+        <TopSection />
+        <SecondSection />
+      </div>
     </div>
   );
 }

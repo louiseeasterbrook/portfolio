c8156d7db91518b2b1df29985c817a09b46f3a19
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-09-15 23:19:36 +1000

    added responsive nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,32 @@
 import "./Navbar.css";
 // import { Link } from "react-router-dom";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMouse } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  // console.log(toggleMenu);
+  // console.log(screenWidth);
+
+  const navToggle = () => {
+    setToggleMenu(!toggleMenu);
+  };
+  useEffect(() => {
+    //Change the screen with value
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+      //set toogle menu to flase if screen expands past 600px
+      if (screenWidth > 600) {
+        setToggleMenu(false);
+      }
+    };
+    //everytime the window is resizes the changeWidth function is called
+    window.addEventListener("resize", changeWidth);
+  }, [screenWidth]);
+
   return (
     <div className="nav-outer">
       <div className="nav-inner">
@@ -16,10 +38,34 @@ const Navbar = () => {
         </div>
 
         <div className="nav-links">
-          <a href="">Reusme</a>
+          <a href="">Resume</a>
           <a href="">Github</a>
         </div>
+        <div className="nav-btn" onClick={navToggle}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
+
+      {toggleMenu && screenWidth < 600 && (
+        <div className="menu-holder">
+          <div className="menu-inner">
+            <ul>
+              <li>
+                <a href="" className="menu-link">
+                  Resume
+                </a>
+              </li>
+              <li>
+                <a href="" className="menu-link">
+                  Github
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };

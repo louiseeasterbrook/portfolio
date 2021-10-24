85ddcde9bcdd2d14b0301ef4098e7e44c24f6ce8
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-10-25 03:01:46 +1100

    remove Topsection from App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import SecondSection from "./screens/SecondSection";
-import TopSection from "./screens/TopSection";
 
 function App() {
   return (
     <div>
       <div className="screen">
         <Navbar />
-        <TopSection />
         <SecondSection />
       </div>
     </div>

334b5ab93cf6490ba7ffdd8361c4323191ba84ec
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-11-28 22:57:16 +1100

    create loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+import "./Loading.css";
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="load-square">
+        <h1>LE</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;

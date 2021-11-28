bbc403b7c35cccd13ec8529e884737e78bb6d160
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-11-28 22:53:56 +1100

    change layout of website

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
+
 import SecondSection from "./screens/SecondSection";
+import TopSection from "./screens/TopSection";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Loading from "./components/Loading";
+import { useRef } from "react";
 
 function App() {
+  const projectsRef = useRef(null);
   return (
     <div>
       <div className="screen">
-        <Navbar />
-        <SecondSection />
+        <Loading />
+        <Header />
+        <TopSection projectsRef={projectsRef} />
+        <SecondSection projectsRef={projectsRef} />
+        <Footer />
       </div>
     </div>
   );
